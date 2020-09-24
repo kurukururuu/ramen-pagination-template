df04c5ab3be4c81ae9f3dbd6744be1ecd614abf7
kurukururuu <apriansyah.fajar94@gmail.com>
2020-09-24 14:41:51 +0700

    v0.1.2 updated with newest ramenbox adonis js v1.2.42

--- docs/App.vue
+++ docs/App.vue
@@ -1,57 +1,66 @@
 <template>
   <div id="app">
-		<div class="wrapper">
-			<PaginationComponent
-				:data="list"
-				text-nav
-				class="row content-centered"
-				@pageChanged="paginationChanged"
-				/>
-		</div>
+    <div class="wrapper">
+      <div>
+        Page :
+        <div class="bold">{{ (list.pagination || {}).page }}</div>
+      </div>
+      <PaginationComponent
+        :data="list"
+        text-nav
+        class="row content-centered"
+        @pageChanged="paginationChanged"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import PaginationComponent from '../src'
-import axios from 'axios'
+import PaginationComponent from "../src";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-		PaginationComponent
-	},
-	data() {
-		return {
-			list: {}
-		}
-	},
-	mounted() {
-		this.fetchItems({limit:5})
-	},
-	methods: {
-		async fetchItems(args) {
-			let query = ''
-			if (args) {
-				query += '?orderBy=<created_at'
-				if (args.page) {
-					query += `&page=${args.page}`
-				}
-				if (args.limit) {
-					query += `&limit=${args.limit}`
-				}
-			}
-			let url = 'https://api.khubforjustice.org/api/publications' + query
-			const result = await axios.get(url)
+    PaginationComponent,
+  },
+  data() {
+    return {
+      list: {},
+      limit: 1,
+      page: 1,
+    };
+  },
+  mounted() {
+    this.fetchItems({ limit: this.limit, page: this.page });
+  },
+  methods: {
+    async fetchItems(args) {
+      let query = "with=district,category,parent,institution,task";
+      if (args) {
+        // query += 'orderBy=<created_at'
+        if (args.page) {
+          query += `&page=${args.page}`;
+        }
+        if (args.limit) {
+          query += `&limit=${args.limit}`;
+        }
+      }
+      let url =
+        "https://api-dot-labuanbajo-ordent.et.r.appspot.com/api/v1/projects?" +
+        query;
+      console.log(url);
+      const result = await axios.get(url);
 
-			this.list = result.data
-			// console.log('res', result)
-		},
-		paginationChanged(e) {
-			// console.log('page changed', e)
-			this.fetchItems({page:e.page_destination, limit:5})
-		}
-	}
-}
+      this.list = result.data;
+      // console.log('res', result)
+    },
+    paginationChanged(e) {
+      // console.log('page changed', e)
+      this.fetchItems({ page: e.page_destination, limit: this.limit });
+    },
+  },
+};
 </script>
 
 <style>
@@ -61,11 +70,11 @@ export default {
   /* -moz-osx-font-smoothing: grayscale; */
   /* text-align: center; */
   /* color: #2c3e50; */
-	margin-top: 60px;
+  margin-top: 60px;
 }
 
 .wrapper {
-	/* width: 600px; */
-	margin: 0 auto;
+  /* width: 600px; */
+  margin: 0 auto;
 }
 </style>
--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,255 +1,291 @@
 <template>
-	<div>
-		<div class="row pagination align-center">
-			<!-- prev button -->
-			<a
-				v-if="data.meta"
-				:class="'pagination navigation ' + (currentPage === 1 || data.meta.pagination.count == 0 ? '' : 'visible')"
-				@click="actionPrevious(currentPage)"
-				>
-				<span v-if="textNav">{{ prevText }}</span>
-				<span v-else>&lt;</span>
-			</a>
+  <div>
+    <div class="row pagination align-center">
+      <!-- prev button -->
+      <a
+        v-if="data.pagination"
+        :class="
+          'pagination navigation ' +
+            (currentPage === 1 || data.pagination.total == 0 ? '' : 'visible')
+        "
+        @click="actionPrevious(currentPage)"
+      >
+        <span v-if="textNav">{{ prevText }}</span>
+        <span v-else>&lt;</span>
+      </a>
 
-			<!-- numbers -->
-			<div v-if="data.meta">
-				<div v-if="data.meta.pagination.total_pages > 1">
-					<div v-if="data.meta.pagination.total_pages > 5">
-						<div class="row align-center">
-							<a
-								v-if="currentPage > 3"
-								:class="'pagination'"
-								:title="1"
-								@click="goToPage"
-								>
-								{{ firstText }}
-							</a>
-							<div v-if="currentPage > 3">
-								<span class="dot">. . .</span>
-							</div>
-							<a
-								v-for="(i, index) in paginationTemplate"
-								:key="index"
-								:class="currentPage === paginationTemplate[index] ? 'active' : 'not-active'"
-								:disabled="currentPage === paginationTemplate[index] ? true : false"
-								@click="goToPage"
-								:title="paginationTemplate[index]"
-								>
-								{{ paginationTemplate[index] }}
-							</a>
+      <!-- numbers -->
+      <div v-if="data.pagination">
+        <div v-if="data.pagination.lastPage > 1">
+          <div v-if="data.pagination.lastPage > 5">
+            <div class="row align-center">
+              <a
+                v-if="currentPage > 3"
+                :class="'pagination'"
+                :title="1"
+                @click="goToPage"
+              >
+                {{ firstText }}
+              </a>
+              <div v-if="currentPage > 3">
+                <span class="dot">. . .</span>
+              </div>
+              <a
+                v-for="(i, index) in paginationTemplate"
+                :key="index"
+                :class="
+                  currentPage === paginationTemplate[index]
+                    ? 'active'
+                    : 'not-active'
+                "
+                :disabled="
+                  currentPage === paginationTemplate[index] ? true : false
+                "
+                @click="goToPage"
+                :title="paginationTemplate[index]"
+              >
+                {{ paginationTemplate[index] }}
+              </a>
 
-							<div v-if="currentPage < data.meta.pagination.total_pages-2">
-								<span class="dot">. . .</span>
-							</div>
+              <div v-if="currentPage < data.pagination.lastPage - 2">
+                <span class="dot">. . .</span>
+              </div>
 
-							<a
-								v-if="currentPage < data.meta.pagination.total_pages-2"
-								:class="'pagination'"
-								:title="data.meta.pagination.total_pages"
-								@click="goToPage"
-								>
-								{{ lastText }}
-							</a>
-						</div>
-					</div>
-					
-					<!-- if total pages < 5 -->
-					<div v-else>
-						<a
-							v-for="(i, index) in paginationTemplate"
-							:key="index"
-							:title="paginationTemplate[index]"
-							:class="currentPage == paginationTemplate[index] ? 'active' : 'not-active'"
-							:disabled="currentPage === paginationTemplate[index] ? true : false"
-							@click="goToPage"
-							>
-							{{ paginationTemplate[index] }}
-						</a>
-					</div>
-				</div>
-			</div>
+              <a
+                v-if="currentPage < data.pagination.lastPage - 2"
+                :class="'pagination'"
+                :title="data.pagination.lastPage"
+                @click="goToPage"
+              >
+                {{ lastText }}
+              </a>
+            </div>
+          </div>
 
-			<!-- next button -->
-			<a
-				v-if="data.meta"
-				:class="'pagination navigation ' + (currentPage === data.meta.pagination.total_pages || data.meta.pagination.count == 0 ? '' : 'visible')"
-				@click="actionNext(currentPage, data.meta.pagination.total_pages)"
-				>
-				<span v-if="textNav">{{ nextText }}</span>
-				<span v-else>&gt;</span>
-			</a>
-		</div>
-		<br>
-		<div v-if="!data.meta">{{ emptyText }}</div>
-	</div>
+          <!-- if total pages < 5 -->
+          <div v-else>
+            <a
+              v-for="(i, index) in paginationTemplate"
+              :key="index"
+              :title="paginationTemplate[index]"
+              :class="
+                currentPage == paginationTemplate[index]
+                  ? 'active'
+                  : 'not-active'
+              "
+              :disabled="
+                currentPage === paginationTemplate[index] ? true : false
+              "
+              @click="goToPage"
+            >
+              {{ paginationTemplate[index] }}
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- next button -->
+      <a
+        v-if="data.pagination"
+        :class="
+          'pagination navigation ' +
+            (currentPage === data.pagination.lastPage ||
+            data.pagination.total == 0
+              ? ''
+              : 'visible')
+        "
+        @click="actionNext(currentPage, data.pagination.lastPage)"
+      >
+        <span v-if="textNav">{{ nextText }}</span>
+        <span v-else>&gt;</span>
+      </a>
+    </div>
+    <br />
+    <div v-if="!data.pagination">{{ emptyText }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		data: {
-			type: Object,
-			default() {
-				return {
-					data: {},
-					meta: {}
-				}
-			}
-		},
-		textNav: {
-			type: Boolean,
-			default() {
-				return false
-			}
-		},
-		firstText: {
-			type: String,
-			default() {
-				return 'First Page'
-			}
-		},
-		lastText: {
-			type: String,
-			default() {
-				return 'Last Page'
-			}
-		},
-		prevText: {
-			type: String,
-			default() {
-				return 'Previous'
-			}
-		},
-		nextText: {
-			type: String,
-			default() {
-				return 'Next'
-			}
-		},
-		emptyText: {
-			type: String,
-			default() {
-				return 'No Data Available'
-			}
-		},
-	},
-	computed: {
-		currentPage() {
-			return this.data.meta.pagination.current_page || 1
-		},
-		paginationTemplate() {
-			const arr = []
-			const meta = this.data.meta
-			const b = this.currentPage
-			if (meta.pagination) {
-				if (meta.pagination.total_pages < 6) {
-					for (let i = 0; i < 5; i++) {
-						if (i < meta.pagination.total_pages) {
-							arr.push(i + 1)
-						}
-					}
-				} else if (b < 3) {
-					for (let i = 0; i < 5; i++) {
-						if (i <= meta.pagination.total_pages) {
-							arr.push(i + 1)
-						}
-					}
-				} else { // exp: b=10, total_pages = 10
-					let temp = b - 2 // temp = 8
-					for (let i = 0; i < 5; i++) {
-						if (temp <= meta.pagination.total_pages) { // 8 < 10 true
-							arr.push(b + (i - 2))
-							temp++
-						}
-					}
-				}
-			}
-			return arr
-		}
-	},
-	mounted() {
-	},
-	methods: {
-		actionPrevious() {
-			// this.currentPage--
-			this.$emit('pageChanged', { action: 'previous', page_destination: this.currentPage - 1 })
-		},
-		actionNext() {
-			// this.currentPage++
-			this.$emit('pageChanged', { action: 'next', page_destination: this.currentPage + 1 })
-		},
-		goToPage(e) {
-			let temp = e.target.title
-			if (temp !== this.currentPage) {
-				this.$emit('pageChanged', { action: 'page-changed', page_destination: Number(temp) })
-			}
-		}
-	}
-}
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {
+          data: {},
+          pagination: {},
+        };
+      },
+    },
+    textNav: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+    firstText: {
+      type: String,
+      default() {
+        return "First Page";
+      },
+    },
+    lastText: {
+      type: String,
+      default() {
+        return "Last Page";
+      },
+    },
+    prevText: {
+      type: String,
+      default() {
+        return "Previous";
+      },
+    },
+    nextText: {
+      type: String,
+      default() {
+        return "Next";
+      },
+    },
+    emptyText: {
+      type: String,
+      default() {
+        return "No Data Available";
+      },
+    },
+  },
+  computed: {
+    currentPage() {
+      return this.data.pagination.page || 1;
+    },
+    paginationTemplate() {
+      const arr = [];
+      const meta = this.data;
+      const b = this.currentPage;
+      if (meta.pagination) {
+        if (meta.pagination.lastPage < 6) {
+          for (let i = 0; i < 5; i++) {
+            if (i < meta.pagination.lastPage) {
+              arr.push(i + 1);
+            }
+          }
+        } else if (b < 3) {
+          for (let i = 0; i < 5; i++) {
+            if (i <= meta.pagination.lastPage) {
+              arr.push(i + 1);
+            }
+          }
+        } else {
+          // exp: b=10, lastPage = 10
+          let temp = b - 2; // temp = 8
+          for (let i = 0; i < 5; i++) {
+            if (temp <= meta.pagination.lastPage) {
+              // 8 < 10 true
+              arr.push(b + (i - 2));
+              temp++;
+            }
+          }
+        }
+      }
+      return arr;
+    },
+  },
+  mounted() {},
+  methods: {
+    actionPrevious() {
+      // this.currentPage--
+      this.$emit("pageChanged", {
+        action: "previous",
+        page_destination: this.currentPage - 1,
+      });
+    },
+    actionNext() {
+      // this.currentPage++
+      this.$emit("pageChanged", {
+        action: "next",
+        page_destination: this.currentPage + 1,
+      });
+    },
+    goToPage(e) {
+      let temp = e.target.title;
+      if (temp !== this.currentPage) {
+        this.$emit("pageChanged", {
+          action: "page-changed",
+          page_destination: Number(temp),
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-	.pagination {
-		a {
-			text-align: center;
-			color: black;
-			background-color: white;
-			border: solid 0.5px #333;
-			border-radius: 5px;
-			padding: 7px 13px;
-			text-decoration: none;
-			transition: background-color .3s;
-			margin: 4px;
-			&.active {
-				background-color: #333;
-				color: white;
-				border-radius: 5px;
-				cursor: auto;
-			}
-			&:hover:not(.active) {
-				background-color: #F4F5F7;
-				color: black;
-				border-radius: 5px;
-				cursor: pointer;
-			}
-		}
-	}
-	.navigation {
-		display: none;
-		&.visible {
-			display: block;
-		}
-	}
-	.dot {
-		text-align: center;
-		color: black;
-		/* background-color: #bbbbbb; */
-		border-radius: 5px;
-		padding: 7px 8px;
-		text-decoration: none;
-		transition: background-color .3s;
-		margin: 4px;
-	}
+.pagination {
+  a {
+    text-align: center;
+    color: black;
+    background-color: white;
+    border: solid 0.5px #333;
+    border-radius: 5px;
+    padding: 7px 13px;
+    text-decoration: none;
+    transition: background-color 0.3s;
+    margin: 4px;
+    &.active {
+      background-color: #333;
+      color: white;
+      border-radius: 5px;
+      cursor: auto;
+    }
+    &:hover:not(.active) {
+      background-color: #f4f5f7;
+      color: black;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  }
+}
+.navigation {
+  display: none;
+  &.visible {
+    display: block;
+  }
+}
+.dot {
+  text-align: center;
+  color: black;
+  /* background-color: #bbbbbb; */
+  border-radius: 5px;
+  padding: 7px 8px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  margin: 4px;
+}
 
-	#mobile {
-		display: none;
-	}
-	// responsive
-	@media (min-width: 0px) and (max-width: 768px) { // small-large phone (xs)
-		#mobile {
-			display: block;
-			min-width: 200px;
-		}
-		#desktop {
-			display: none;
-		}
-	}
-	@media (min-width: 769px) and (max-width: 1023px) { // tablet  (xs)
-	}
-	@media (min-width: 1024px) and (max-width: 1215px) { // desktop (xs)
-	}
-	@media (min-width: 1216px) and (max-width: 1407px) { // widescreen (xs)
-	}
-	@media (min-width: 1408px) { // Full HD (xs)
-	}
+#mobile {
+  display: none;
+}
+// responsive
+@media (min-width: 0px) and (max-width: 768px) {
+  // small-large phone (xs)
+  #mobile {
+    display: block;
+    min-width: 200px;
+  }
+  #desktop {
+    display: none;
+  }
+}
+@media (min-width: 769px) and (max-width: 1023px) {
+  // tablet  (xs)
+}
+@media (min-width: 1024px) and (max-width: 1215px) {
+  // desktop (xs)
+}
+@media (min-width: 1216px) and (max-width: 1407px) {
+  // widescreen (xs)
+}
+@media (min-width: 1408px) {
+  // Full HD (xs)
+}
 </style>
